c54f9d93bdbcce081b34853b5882c8332fe6946f
Colin Eckert <colineckert10@gmail.com>
2025-10-20 06:06:53 -0700

    Connect to amqp on server start

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,15 @@
+import amqp from "amqplib";
+
 async function main() {
   console.log("Starting Peril server...");
+
+  const connection = "amqp://guest:guest@localhost:5672/";
+  amqp.connect(connection);
+  console.log("Connection successful!");
+
+  process.on("exit", () => {
+    console.log("Shutting down connection...");
+  });
 }
 
 main().catch((err) => {
