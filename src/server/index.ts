fd029efc297963335ac922f275344f6e843be7b6
Colin Eckert <colineckert10@gmail.com>
2025-10-20 20:39:32 -0700

    create dockerfile for stomp support

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,15 +1,22 @@
 import amqp from "amqplib";
 
 async function main() {
-  console.log("Starting Peril server...");
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(rabbitConnString);
+  console.log("Peril game server connected to RabbitMQ!");
 
-  const connection = "amqp://guest:guest@localhost:5672/";
-  amqp.connect(connection);
-  console.log("Connection successful!");
-
-  process.on("exit", () => {
-    console.log("Shutting down connection...");
-  });
+  ["SIGINT", "SIGTERM"].forEach((signal) =>
+    process.on(signal, async () => {
+      try {
+        await conn.close();
+        console.log("RabbitMQ connection closed.");
+      } catch (err) {
+        console.error("Error closing RabbitMQ connection:", err);
+      } finally {
+        process.exit(0);
+      }
+    }),
+  );
 }
 
 main().catch((err) => {
